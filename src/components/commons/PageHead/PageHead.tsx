e6efdbb8435c45a756e86cb61c80255da807e1d0
StaryOne <stanyslausharyone@gmail.com>
2025-01-28 01:16:43 +0700

    feat: init project

---
+++ src/components/commons/PageHead/PageHead.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+interface PropTypes {
+  title?: string;
+}
+
+const PageHead = (props: PropTypes) => {
+  const {title = 'Acara'} = props;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+  )
+}
+
+export default PageHead;
